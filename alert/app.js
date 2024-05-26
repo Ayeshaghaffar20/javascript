be71ca6be2778e65c0e9c41edee465d8f46ec985
Ayesha Ghaffar <gayesha759@gmail.com>
2024-05-26 19:31:49 +0500

    initial

---
+++ alert/app.js
@@ -0,0 +1,8 @@
+alert("Welcome to our website! We're glad you're here.");
+alert("Error! Please Enter a Valid password");
+console.log("hello...i can run js through browser's developer console");
+alert("Welcome to Js land \n happy coding");
+alert("Enter a value of a!");
+let a = prompt("Enter a Here")
+document.write(a);
+alert("You entered a of type " + (typeof a));
